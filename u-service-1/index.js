31ca8fad8ee4889dd07134c471a629890d7d25a3
Ben Stanley <29062967+benjamistan@users.noreply.github.com>
2021-02-15 15:30:23 +0100

    basic functionality complete

--- u-service-1/index.js
+++ u-service-1/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
+app.use(bodyParser.json());
 
-// post to second microservice
-app.post('/api', (req, res) => {
+app.post('/api', async (req, res) => {
+	console.log(
+		'received string is: ',
+		JSON.parse(JSON.stringify(req.body.message))
+	);
+	const reverseText = await axios.post('http://localhost:6000/reverse', {
+		message: req.body.message,
+	});
+
+	console.log('reversed string is: ', reverseText.data.message);
+
 	const rand = Math.random().toPrecision(8);
-	res.json({ rand: rand });
+	console.log('random number is:   ', rand);
+
+	res.json({ message: reverseText.data.message, rand: rand });
 });
 
 const PORT = process.env.PORT || 5000;
